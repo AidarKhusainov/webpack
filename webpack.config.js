51047e35dc38dfad2a3188631ef68e7c7bb5b3c4
Aidar Khusainov <husainov.a@rusgis.com>
2018-10-28 12:35:14 +0500

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+let path = require('path');
+
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    devServer: {
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // exlude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = (env, options) => {
+    let production = options.mode === 'production';
+
+    conf.devtool = production ? false : 'eval-sourcemap';
+
+    return conf;
+}
